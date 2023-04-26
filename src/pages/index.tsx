8723e5491d335b7eb22364df149eed25eaa5dd5d
Subham <97989643+Subham-Maity@users.noreply.github.com>
2023-04-26 16:17:25 +0530

    Remove CreditCard Style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,18 @@
 import { useState, useEffect } from "react";
-import { PaymentForm, CreditCard } from "react-square-web-payments-sdk";
+import {
+  GooglePay,
+  PaymentForm,
+  CreditCard,
+} from "react-square-web-payments-sdk";
 
 export default function Home() {
   const [amount, setAmount] = useState<number>(0);
   return (
-    <div className="container mx-auto py-12 px-12 max-sm:px-6 flex flex-col gap-4">
+    <div className="container mx-auto py-12 px-12 max-sm:px-6 flex flex-col gap-4 bg-gray-800 text-white">
       <input
         type="number"
         placeholder="Amount"
-        className="outline-none bg-white border-2 rounded-md px-4 py-2 text-xl border-blue-400 "
+        className="outline-none bg-gray-700 border-2 rounded-md px-4 py-2 text-xl border-purple-400 text-white"
         value={amount}
         onChange={(e: any) => {
           setAmount(e.target.value);
@@ -30,34 +34,12 @@ export default function Home() {
           console.log(await response.json());
         }}
         locationId="LB6KD73BNFPA9"
+        className="rounded-md"
       >
-        <CreditCard
-          buttonProps={{
-            css: {
-              "[data-theme='dark'] &": {
-                backgroundColor: "#61dafb",
-                color: "var(--ifm-color-emphasis-100)",
-                "&:hover": {
-                  backgroundColor: "#0091ea",
-                },
-              },
-              backgroundColor: "#771520",
-              fontSize: "14px",
-              color: "#fff",
-              "&:hover": {
-                backgroundColor: "#530f16",
-              },
-            },
-          }}
-          style={{
-            input: {
-              fontSize: "14px",
-            },
-            "input::placeholder": {
-              color: "#771520",
-            },
-          }}
-        />
+        {" "}
+        <div>
+          <CreditCard className="rounded-md" />
+        </div>
       </PaymentForm>
     </div>
   );
